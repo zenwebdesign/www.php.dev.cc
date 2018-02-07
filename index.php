193c80aabc0f5b557ce3f4b93c5e94666c221134
zenwebdesign <36109281+zenwebdesign@users.noreply.github.com>
2018-02-07 14:44:49 +0200

    pushin'

--- index.php
+++ index.php
@@ -3,11 +3,38 @@
 <body>
 
 <h1>My first PHP page</h1>
+<h5> Hello from html</h5>
 
 <?php
+
 $hello = "Hello World! olala!!";
-echo $hello;
 echo "<br>";
+print "da, merge! $hello";
+echo "<br>da, merge si asa " .  $hello , "^_^";
+echo "This " . "string ", "was ". "made ", "with multiple parameters.";
+var_dump($hello);
+$cars = array("Volvo","BMW","Toyota");
+var_dump($cars);
+define("GREETING", "<br>Welcome to W3Schools.com!", true);
+echo greeting;
+class Car {
+    function Car() {
+        $this->model = "HONDAAA";
+        $this->PRET = "GRATIS";
+        echo GReeTING, "<br>";
+    }
+}
+// create an object
+$herbie = new Car();
+
+// show object properties
+echo $herbie->PRET;
+echo strrev("zlol>rb<>rb<");
+echo "<br>";
+echo strrev($hello);
+
+
+
 
 ?>
 
